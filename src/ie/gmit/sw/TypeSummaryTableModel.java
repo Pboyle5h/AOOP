53d0bd1277cc98c9af56e731aff2f1c8d36d4db5
pboyle5h <pauricboylegti@gmail.com>
2017-01-10 22:42:35 +0000

    Changed a few things

--- src/ie/gmit/sw/TypeSummaryTableModel.java
+++ src/ie/gmit/sw/TypeSummaryTableModel.java
@@ -3,16 +3,17 @@ package ie.gmit.sw;
 import javax.swing.table.*;
 public class TypeSummaryTableModel extends AbstractTableModel{
 	private static final long serialVersionUID = 777L;
-	private String[] cols = {"Class Name", "Out Degree", "In Degree", "Stability"};
+	private String[] cols = {"Class Name", "Stability", "out Degree", "In Degree"};
 	public static Object[][] data = new Object[ReceiveJar.Metrics.size()][4];
 	
 	public static Object[][] getMetricData(){
         int i = 0;
         for(metric m : ReceiveJar.Metrics.values()){            
-            data[i][0] = m.getName();  
-            data[i][1] = m.getOutDegree();  
-            data[i][2] = m.getInDegree(); 
-            data[i][3] = m.getStability(); 
+            data[i][0] = m.getName();
+            data[i][1] = m.getStability();
+            data[i][2] = m.getOutDegree();  
+            data[i][3] = m.getInDegree(); 
+            
             i++;
         } 
 
@@ -20,17 +21,7 @@ public class TypeSummaryTableModel extends AbstractTableModel{
 
     } 
 	
-	public TypeSummaryTableModel(){
-	    super();
 
-	  
-    }
-
-    public void setTableData(Object[][] data){
-
-	    this.data = data;
-
-    } 
 	public int getColumnCount() {
         return cols.length;
     }
